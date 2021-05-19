0490034d0e5bfced4c2da39b57e491773282c606
starmpcc <kjune0322@naver.com>
2021-05-20 00:38:26 +0900

    Implement Image Loading

--- src/components/food_diary/food_diary_main.js
+++ src/components/food_diary/food_diary_main.js
@@ -29,22 +29,37 @@ function FBSelect(arg) {
 }
 
 
-function Render_pictures(uid) {
-    firebase.database().ref(uid+"/feeds/").get().then((snapshot) =>{
-        console.log(Object.keys(snapshot.val()))
-    })
 
-}
-
-function Render_each(feed){
-    function getImage(feed){
-        var imgkey = firebase.storage().ref(uid+'/feeds/'+feed+'/image').once('value').then(snapshot =>{
-            return snapshot.val()
-        })
-        return firebase.database().ref(uid+'/images/'+imgkey).get().then(snapshot => {return snapshot.val()})
+class Renderer extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            urls:[]
+        }
     }
-    return <div style={{height:"50px", width:"50px"}}><img src={getImage(feed)}/></div>
+
+    componentDidMount(){
+        firebase.database().ref(uid+"/feeds/").get().then((snapshot) =>{
+            const maps = Object.keys(snapshot.val()).map((key) =>{
+                return [snapshot.val()[key]['image'], key]
+            })
+            this.setState({urls:maps})
+        })
+    }
+
+    render(){
+        if (this.state.urls===[]) return <div></div>
+        console.log("Asdf")
+
+        return <div>
+            {this.state.urls.map(map => (
+                <img src={map[0]} key={map[1]} style={{width:"100px", height:"100px"}}/>
+            ))}
+            </div>
+        
+    }
 }
+
 
 
 //Upload file
@@ -54,9 +69,16 @@ function Upload_file(){
         if (file==null) return
         const feedkey_list = String(firebase.database().ref(uid+'/feeds/').push()).split('/')
         const feedkey = feedkey_list[feedkey_list.length -1]
-        const imgref = firebase.storage().ref(uid+'/images/').child(feedkey)
-        imgref.put(file).then(() => {alert("file uploaded")})
-        firebase.database().ref(uid+'/feeds/'+feedkey+"/image").set(imgref.fullPath)
+        const imgref = firebase.storage().ref().child(uid).child('images').child(feedkey)
+        imgref.put(file).then(() => {
+            alert("file uploaded")
+        }).then(() => {
+            firebase.storage().ref().child(uid).child('images').child(feedkey).getDownloadURL().then((value) =>{
+                firebase.database().ref(uid+'/feeds/'+feedkey+"/image").set(value)
+            })
+        })
+
+        
     }, [file])
     const upload = (e) => {
         setfile(tmp => e.target.files[0])
@@ -69,6 +91,7 @@ function Upload_file(){
 export default function DiaryMain(){
 
     const [uid, setuid] = useState("sample_uid")
+
     return (
         <>
             <AppNavBar/>
@@ -77,11 +100,11 @@ export default function DiaryMain(){
                 <div style={{width: '300px'}}>{FBSelect(uid +'/locations')}</div>
                 <div style={{width: '300px'}}>{FBSelect(uid +'/origins')}</div>
             </div>
-            <div style={{width:"500px", height:"500px", border:"1px solid black"}}>
-                <span style={{width:"50px", height:"50px"}}></span>
+            <div id="diary_box" style={{width:"500px", height:"500px", border:"1px solid black"}}>
+                <Renderer/>
             </div>
+
             {Upload_file()}
-            {Render_pictures(uid)}
             <div> This is diary main </div>
         </>
     )
