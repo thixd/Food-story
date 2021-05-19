d81f48742d4c2d4ff1963daf8698573ddb225f3c
starmpcc <kjune0322@naver.com>
2021-05-19 19:54:50 +0900

    Implement upload image

--- src/components/food_diary/food_diary_main.js
+++ src/components/food_diary/food_diary_main.js
@@ -16,7 +16,8 @@ firebase.database().ref(uid +'/origins').set([
     {value:"Italian", label:"Italian"}
 ])
 
-firebase.database().ref(uid+'feeds')
+firebase.storage().ref().child(uid)
+firebase.storage().ref(uid).child('images')
 
 function FBSelect(arg) {
     const loadOptions = () => {
@@ -27,6 +28,43 @@ function FBSelect(arg) {
     return <AsyncSelect defaultOptions loadOptions={loadOptions}></AsyncSelect>
 }
 
+
+function Render_pictures(uid) {
+    firebase.database().ref(uid+"/feeds/").get().then((snapshot) =>{
+        console.log(Object.keys(snapshot.val()))
+    })
+
+}
+
+function Render_each(feed){
+    function getImage(feed){
+        var imgkey = firebase.storage().ref(uid+'/feeds/'+feed+'/image').once('value').then(snapshot =>{
+            return snapshot.val()
+        })
+        return firebase.database().ref(uid+'/images/'+imgkey).get().then(snapshot => {return snapshot.val()})
+    }
+    return <div style={{height:"50px", width:"50px"}}><img src={getImage(feed)}/></div>
+}
+
+
+//Upload file
+function Upload_file(){
+    const [file, setfile] = useState(null)
+    useEffect(() => {
+        if (file==null) return
+        const feedkey_list = String(firebase.database().ref(uid+'/feeds/').push()).split('/')
+        const feedkey = feedkey_list[feedkey_list.length -1]
+        const imgref = firebase.storage().ref(uid+'/images/').child(feedkey)
+        imgref.put(file).then(() => {alert("file uploaded")})
+        firebase.database().ref(uid+'/feeds/'+feedkey+"/image").set(imgref.fullPath)
+    }, [file])
+    const upload = (e) => {
+        setfile(tmp => e.target.files[0])
+    }
+    return <div>
+        <input type="file" onChange={upload}/>
+    </div>
+}
 
 export default function DiaryMain(){
 
@@ -42,6 +80,8 @@ export default function DiaryMain(){
             <div style={{width:"500px", height:"500px", border:"1px solid black"}}>
                 <span style={{width:"50px", height:"50px"}}></span>
             </div>
+            {Upload_file()}
+            {Render_pictures(uid)}
             <div> This is diary main </div>
         </>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/database"
+import "firebase/storage"
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -9,7 +10,8 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL: "https://foodstory-c6226-default-rtdb.asia-southeast1.firebasedatabase.app"
 })
 
 export const auth = app.auth()
