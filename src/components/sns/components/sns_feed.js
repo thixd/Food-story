8354fdb4d7c708794873227c88a4785abea9f0ed
yurangja99 <sang990701@gmail.com>
2021-05-25 22:06:29 +0900

    (#2) feeds

--- src/components/sns/components/sns_feed.js
+++ src/components/sns/components/sns_feed.js
@@ -1,24 +1,81 @@
 import React, { useState, useEffect } from "react";
 import "./sns_feed.css";
+import firebase from "../../../firebase";
 
-export default function SnsFeed({feed}){
+export default function SnsFeed({feedId, feedInfo}){
   // Whether range dropdown is opened
   const [rangeOpened, setRangeOpened] = useState(false);
 
   // Data
-  const [feedInfo, setFeedInfo] = useState(feed);
-
-  // Default feed data
-  const defaultFeed = {
-    profile: "https://firebasestorage.googleapis.com/v0/b/foodstory-c6226.appspot.com/o/static%2Fdefault_profile.jpg?alt=media&token=723ea738-6941-41c1-8a1d-4f26b1dbb88c",
-    nickname: "author",
-    time: "1min",
-    isPrivate: true,
+  const [feed, setFeed] = useState(Object.assign({
+    comments: [], 
+    hashtags: [], 
     image: "https://firebasestorage.googleapis.com/v0/b/foodstory-c6226.appspot.com/o/static%2Ffeed_image.png?alt=media&token=89d471dd-382f-460f-8046-c42affa0f40a", 
-    like: 105,
-    comment: 142,
-    hashtag: "#hashtag1 #hashtag2 #hashtag3"
-  };
+    lat: 0, 
+    lng: 0, 
+    location: "", 
+    origin: "", 
+    reaction: [], 
+    text: "", 
+    user: "sample_uid", 
+    time: "1min", 
+    isPrivate: true
+  }, feedInfo));
+  const [author, setAuthor] = useState({
+    profile: "https://firebasestorage.googleapis.com/v0/b/foodstory-c6226.appspot.com/o/static%2Fdefault_profile.jpg?alt=media&token=723ea738-6941-41c1-8a1d-4f26b1dbb88c",
+    nickname: "author"
+  });
+
+  // Snapshot setting
+  useEffect(() => {
+    firebase.database().ref("Feeds/" + feedId).on("value", (snapshot) => {
+      setFeed((f) => Object.assign({}, f, snapshot.val()));
+    });
+  }, []);
+
+  // Load user data
+  useEffect(() => {
+    firebase.database().ref(feed.user).get().then((snapshot) => {
+      if(snapshot.exists()) {
+        var authorVal = snapshot.val();
+        setAuthor(aut => Object.assign({}, aut, { 
+          profile: authorVal.profile, 
+          nickname: authorVal.nickname 
+        }));
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+    console.log(feed);
+  }, [feed ]);
+
+  // range handler
+  function rangeChangeHandler(newRange) {
+    // local change
+    var newFeed = Object.assign({}, feed, { isPrivate: newRange }); 
+    setFeed(newFeed);
+    setRangeOpened(!rangeOpened);
+    
+    // firebase change
+    var updates = {};
+    updates["Feeds/" + feedId] = newFeed;
+    firebase.database().ref().update(updates);
+  }
+
+  // reaction handler
+  function reactionHandler() {
+    // add if not reacted, and remove if reacted
+    var reactionKey = Object.keys(feed.reaction).find((r) => feed.reaction[r] === author.nickname);
+    if(reactionKey !== undefined) {
+      // remove
+      firebase.database().ref("Feeds/" + feedId + "/reaction/" + reactionKey).remove();
+    } else {
+      // add
+      firebase.database().ref("Feeds/" + feedId + "/reaction").push(author.nickname);
+    }
+  }
 
   // Other images
   const privateIcon = "https://firebasestorage.googleapis.com/v0/b/foodstory-c6226.appspot.com/o/static%2Fprivate_icon.png?alt=media&token=57b523de-f0ae-4877-ae30-5d5374d16cc3";
@@ -26,32 +83,25 @@ export default function SnsFeed({feed}){
   const likeIcon = "https://firebasestorage.googleapis.com/v0/b/foodstory-c6226.appspot.com/o/static%2Flike_icon.png?alt=media&token=9bfe50a4-9d15-4be4-bbca-6a13f920f8af";
   const commentIcon = "https://firebasestorage.googleapis.com/v0/b/foodstory-c6226.appspot.com/o/static%2Fcomment_icon.png?alt=media&token=d47b813b-60c8-4fc2-8e16-aa2798f39fb4";
 
-  useEffect(() => {
-    console.log("rangeOpened");
-    console.log(rangeOpened);
-    console.log("feedInfo");
-    console.log(feedInfo);
-  }, [feedInfo]);
-
   return (
     <div className="feed">
       <div className="feed_header">
         <div className="feed_info">
           <img className="feed_info_image" 
-            src={feedInfo.profile ? feedInfo.profile : defaultFeed.profile}
+            src={author.profile}
             alt="profile"
           />
           <div className="feed_info_text">
             <span className="feed_info_author">
-              {feedInfo.nickname ? feedInfo.nickname : defaultFeed.nickname}
+              {author.nickname}
             </span>
             <span className="feed_info_time">
-              {feedInfo.time ? feedInfo.time : defaultFeed.time}
+              {feed.time}, {feed.location}
             </span>
           </div>
         </div>
         <div className="feed_range">
-          {(feedInfo.isPrivate !== undefined ? feedInfo.isPrivate : defaultFeed.isPrivate) ? (
+          {feed.isPrivate === true ? (
             <div className="feed_range_selected"
               onClick={() => setRangeOpened(!rangeOpened)} >
               <img className="feed_range_image" 
@@ -75,7 +125,7 @@ export default function SnsFeed({feed}){
           {rangeOpened ? (
             <div className="feed_range_option_list">
               <div className="feed_range_option"
-                onClick={() => {setRangeOpened(!rangeOpened); var newFeed = Object.assign({}, feedInfo); newFeed.isPrivate = true; setFeedInfo(newFeed)}} >
+                onClick={() => rangeChangeHandler(true)} >
                 <img className="feed_range_image" 
                   src={privateIcon}
                   alt="private"
@@ -83,7 +133,7 @@ export default function SnsFeed({feed}){
                 <span>Private</span>
               </div>
               <div className="feed_range_option"
-                onClick={() => {setRangeOpened(!rangeOpened); var newFeed = Object.assign({}, feedInfo); newFeed.isPrivate = false; setFeedInfo(newFeed)}} >
+                onClick={() => rangeChangeHandler(false)} >
                 <img className="feed_range_image" 
                   src={publicIcon}
                   alt="public"
@@ -95,24 +145,31 @@ export default function SnsFeed({feed}){
         </div>
       </div>
       <img className="feed_image" 
-        src={feedInfo.image ? feedInfo.image : defaultFeed.image}
+        src={feed.image}
         alt="feed"
       />
+      <span className="feed_hashtag">
+        #{feed.origin} #{feed.hashtags.join(" #")}
+      </span>
+      <span className="feed_text">
+        {feed.text}
+      </span>
       <div className="feed_reaction">
-        <span className="feed_reaction_like">105</span>
-        <img className="feed_reaction_like_image" 
-          src={likeIcon}
-          alt="like"
-        />
-        <span className="feed_reaction_comment">142</span>
+        <div className={Object.values(feed.reaction).find((v) => v === author.nickname) ? "feed_reaction_like_enabled" : "feed_reaction_like_disabled"}
+          onClick={() => reactionHandler()}>
+          <span className="feed_reaction_like">{Object.keys(feed.reaction).length}</span>
+          <img className="feed_reaction_like_image" 
+            src={likeIcon}
+            alt="like"
+          />
+        </div>
+        <span className="feed_reaction_comment">{Object.keys(feed.comments).length}</span>
         <img className="feed_reaction_comment_image" 
           src={commentIcon}
           alt="comment"
         />
       </div>
-      <span className="feed_hashtag">
-        {feedInfo.hashtag ? feedInfo.hashtag : defaultFeed.hashtag}
-      </span>
+      
     </div>
   );
 }
